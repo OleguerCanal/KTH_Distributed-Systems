d4a1a9bb6cac54e12d3c1aa32d894b8eacf871c6
OleguerCanal <oleguer.canal@hotmail.com>
2020-02-08 16:31:35 +0100

    Added timing functions

--- hw_1/mandelbrot_multiprocess.c
+++ hw_1/mandelbrot_multiprocess.c
@@ -49,11 +49,10 @@ unsigned char get_pixel_value(int i, int j, double area, int max_iterations) {
 }
 
 int main() {
-    printf("GO! \n");
-
     // Mandelbrot parameters
-    double area = 3;
-    int max_iterations = 1000;
+    double area = 2;
+    int max_iterations = 2000;
+    char text_img_path[] = "image.txt";
 
     // Multiprocess vars
     int size;  // Number of processes
@@ -65,37 +64,44 @@ int main() {
     status = MPI_Comm_size(MPI_COMM_WORLD, &size);  // Get size
     status = MPI_Comm_rank(MPI_COMM_WORLD, &rank);  // Get ranks
 
+    // TIMING
+    MPI_Barrier(MPI_COMM_WORLD);
+    double start = MPI_Wtime();
 
-    // Compute corresponding part of image
     int n = N/size;
-    // printf("%d \n", n);
-
-    unsigned char partial_image[n*M];
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < M; j++) {
-            // printf("%d \n", i*M+j);
-            partial_image[i*M+j] = get_pixel_value(rank*n + i, j, area, max_iterations);}
-
-    // printf("okii");
-    
     if (rank == 0) {  // If is master
-        // Create and copy computed values to complete image
-        unsigned char image[N*M];
-        for (int i = 0; i < n*M; i++)
-            image[i] = partial_image[i];
-
-        // printf("ok");
+        // Compute first chunk of image
+        FILE *fp;
+        fp = fopen(text_img_path, "w");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < M; j++)
+                fprintf(fp, "%hhu ", get_pixel_value(i, j, area, max_iterations));
+            fprintf(fp, "\n");
+        }
 
         // Receive from all other processes and append to main image
         unsigned char slave_partial_image[n*M];
         for (int process = 1; process < size; process++) {
             status = MPI_Recv(&slave_partial_image, n*M, MPI_UNSIGNED_CHAR, process, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            for (int i = 0; i < n*M; i++)
-                image[process*n*M + i] = slave_partial_image[i];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < M; ++j)
+                    fprintf(fp, "%hhu ", slave_partial_image[i*M + j]);
+                fprintf(fp, "\n");
+            }
         }
-        save("image.txt", image);
+        fclose(fp);
+
+        // TIMING
+        double end = MPI_Wtime();
+        printf("Execution time: %e seconds.\n", end - start);
     } else {  // If slave
-        // Send partial image 
+        // Compute corresponding part of image
+        unsigned char partial_image[n*M];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < M; j++)
+                partial_image[i*M+j] = get_pixel_value(rank*n + i, j, area, max_iterations);
+        
+        // Send partial image
         status = MPI_Send(&partial_image, n*M, MPI_UNSIGNED_CHAR, 0, tag, MPI_COMM_WORLD);
     }    
     status = MPI_Finalize();
