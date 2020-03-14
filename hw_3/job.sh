d784bc878a2d9c93099a69e13bcd51076ff7320b
OleguerCanal <oleguer.canal@hotmail.com>
2020-03-14 22:55:39 +0100

    Updated job.sh

--- hw_3/job.sh
+++ hw_3/job.sh
@@ -3,7 +3,7 @@
 # time allocation
 #SBATCH -A edu20.SF2568
 # job name
-#SBATCH -J myjob
+#SBATCH -J sorting
 # email notification
 #SBATCH --mail-type=BEGIN,END
 # 10 minutes wall-clock time will be given to this job
@@ -15,11 +15,13 @@
 
 module add i-compilers intelmpi
 
-mpiexec -n 1 odd_even_sort 100000000
-mpiexec -n 2 odd_even_sort 100000000
-mpiexec -n 4 odd_even_sort 100000000
-mpiexec -n 5 odd_even_sort 100000000
-mpiexec -n 8 odd_even_sort 100000000
-mpiexec -n 10 odd_even_sort 100000000
-mpiexec -n 16 odd_even_sort 100000000
-mpiexec -n 20 odd_even_sort 100000000
+mpicc -std=c99 odd_even_sort.c -O3 -o odd_even_sort;
+
+mpirun -np 1 ./odd_even_sort 100000000
+mpirun -np 2 ./odd_even_sort 100000000
+mpirun -np 4 ./odd_even_sort 100000000
+mpirun -np 5 ./odd_even_sort 100000000
+mpirun -np 8 ./odd_even_sort 100000000
+mpirun -np 10 ./odd_even_sort 100000000
+mpirun -np 16 ./odd_even_sort 100000000
+mpirun -np 20 ./odd_even_sort 100000000
