6768aaffe28ef5091d62de5e168825c882380f6c
OleguerCanal <oleguer.canal@hotmail.com>
2020-05-02 01:16:24 +0200

    Fixed bug

--- project/job_test.sh
+++ project/job_test.sh
@@ -9,6 +9,7 @@ cd ..
 WS=500 # World size. Try to keep WS^2/N ~ 100
 
 for i in 1 2 3 4
+do
     for N in 1000 2000 16000 32000 64000 128000 256000
     do
         # Baseline
@@ -31,4 +32,5 @@ for i in 1 2 3 4
         do
             mpirun -n $P build/my_mpi_bin $N $WS $p_x
         done
+    done
 done
